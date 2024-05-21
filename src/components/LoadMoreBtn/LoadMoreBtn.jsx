bdfbac27815d4f6de57e1f71709bcb81f927930f
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-05-21 09:29:12 +0300

    add

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,9 +1,9 @@
 import css from "./LoadMoreBtn.module.css";
 
-export default function LoadMoreBtn(loadMoreImg) {
+export default function LoadMoreBtn({ onClick }) {
   return (
     <div className={css.containerBtn}>
-      <button className={css.btn} type="button" onClick={loadMoreImg}>
+      <button className={css.btn} type="button" onClick={onClick}>
         Load More
       </button>
     </div>
